76ad042df99a1599eda475c69921555ca16a8a41
ayaco0 <zaurusu8569@yahoo.co.jp>
2023-12-14 13:33:32 +0900

    スコアファイルの修正,発話者ごとのEER算出

--- script/eval_ours.py
+++ script/eval_ours.py
@@ -50,11 +50,44 @@ def plot_det_curve(target_scores, nontarget_scores, save_path="research-project-
     plt.savefig(save_path)
     print(f"DET Curve plot saved at: {save_path}")
 
-if __name__  == "__main__":
-    file_path = "research-project-seminar-ASVspoof/result/scores-lfcc-PA-ours.txt"
+def compute_eer_for_each_speaker(file_path):
     scores = read_scores_from_file(file_path)
 
-    # 各行を分割して正常な話者と異なる話者のスコアを取得
+    # 各話者ごとにスコアを保存
+    speaker_scores = {}
+    lines = scores.strip().split('\n')
+
+    for line in lines:
+        parts = line.split()
+        speaker_id = parts[0].split('_')[0]
+        score = float(parts[-1])
+
+        if speaker_id not in speaker_scores:
+            speaker_scores[speaker_id] = {'target': [], 'nontarget': []}
+
+        if parts[0].endswith('_spf00_env20'):
+            speaker_scores[speaker_id]['target'].append(score)
+        elif parts[0].endswith('_spf01_env20'):
+            speaker_scores[speaker_id]['nontarget'].append(score)
+
+    # 各話者ごとに EER を計算
+    for speaker_id, scores in speaker_scores.items():
+        target_scores = np.array(scores['target'])
+        nontarget_scores = np.array(scores['nontarget'])
+
+        if len(target_scores) != 0 and len(nontarget_scores) != 0:
+            eer, threshold = compute_eer(target_scores, nontarget_scores)
+            print(f'Speaker {speaker_id}: EER: {eer}, Threshold: {threshold}')
+        else:
+            print(f'Speaker {speaker_id}: {len(target_scores), len(nontarget_scores)}')
+
+if __name__  == "__main__":
+    file_path = "research-project-seminar-ASVspoof/result/scores-lfcc-PA-ours.txt"
+    # 発話者ごとのEER
+    compute_eer_for_each_speaker(file_path)
+
+    # 全体のEER
+    scores = read_scores_from_file(file_path)
     lines = scores.strip().split('\n')
     target_scores = []  # 正常な話者のスコア
     nontarget_scores = []  # 異なる話者のスコア
@@ -66,14 +99,21 @@ if __name__  == "__main__":
         elif parts[0].endswith('_spf01_env20'):
             nontarget_scores.append(float(parts[-1]))
 
-    # EERと対応する閾値を計算
+    plot_det_curve(np.array(target_scores), np.array(nontarget_scores), save_path="research-project-seminar-ASVspoof/result/det_curve.png")
     eer, threshold = compute_eer(np.array(target_scores), np.array(nontarget_scores))
-    plot_det_curve(np.array(target_scores), np.array(nontarget_scores))
-
-    # 結果を出力
     print(f'EER: {eer}, Threshold: {threshold}')
 
+
 """
+全体の結果
 DET Curve plot saved at: det_curve.png
 EER: 0.25, Threshold: -2.420936400787838
+
+発話者ごとの結果
+Speaker spk10: EER: 0.0, Threshold: -4.164542757730331
+Speaker spk20: EER: 0.1, Threshold: -2.8111762833268017
+Speaker spk30: EER: 0.2, Threshold: -2.4986321838136973
+Speaker spk40: EER: 0.3, Threshold: -1.684924964055071
+Speaker spk50: EER: 0.0, Threshold: -1.4517488078647034
+Speaker spk60: EER: 0.1, Threshold: -2.85419032163945
 """
